0d15ccd13636cb4e728e06fb990bc571c7f9bfd9
igor <igor.kozlov@marlin.tech>
2019-04-06 22:41:07 +0300

    Added CircleImageView and Picasso

--- app/src/main/java/com/example/twitterclient/UserInfoActivity.kt
+++ app/src/main/java/com/example/twitterclient/UserInfoActivity.kt
@@ -1,12 +1,21 @@
 package com.example.twitterclient
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_user_info.*
 
 class UserInfoActivity : AppCompatActivity() {
+    companion object {
+        private const val IMAGE_URL =
+            "https://static.life.ru/posts/2018/07/1132166/gr/north/be8e63e33390c9fd0eff2072999c8384__1440x.jpg"
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_info)
+
+        Picasso.get().load(IMAGE_URL).into(profile_image_view)
     }
 }
